8c80b780504997b26b99ec8e82c6852311d45347
tuan anh <canhazn@gmail.com>
2019-12-28 16:01:43 +0700

    final

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,7 +5,7 @@ window.onload = function () {
     Array.from(arrayPlaceholder).forEach(placeholder => {
 
         var small = placeholder.querySelector('.img-small')
-        console.log(placeholder);
+        // console.log(placeholder);
         // 1: load small image and show it
         var img = new Image();
         img.src = small.src;
@@ -14,12 +14,38 @@ window.onload = function () {
         };
 
         // 2: load large image
-        var imgLarge = new Image();
-        imgLarge.src = placeholder.dataset.large;
-        imgLarge.onload = function () {
-            imgLarge.classList.add('loaded');
-        };
-        placeholder.appendChild(imgLarge);
+        // var imgLarge = new Image();
+        // imgLarge.src = placeholder.dataset.large;
+        // imgLarge.onload = function () {
+        //     imgLarge.classList.add('loaded');
+        // };
+        // placeholder.appendChild(imgLarge);
+
+
     });
+
+    const lazyLoad = target => {
+        const io = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                let placeholder = entry.target;
+                if (entry.isIntersecting) {
+                    console.log('😍');
+                    // 2: load large image
+                    var imgLarge = new Image();
+                    imgLarge.src = placeholder.dataset.large;
+                    imgLarge.onload = function () {
+                        imgLarge.classList.add('loaded');
+                    };
+                    placeholder.appendChild(imgLarge);
+
+                    observer.disconnect();
+                }
+            });
+        });
+
+        io.observe(target)
+    };
+
+    Array.from(arrayPlaceholder).forEach(lazyLoad);
 
 }
